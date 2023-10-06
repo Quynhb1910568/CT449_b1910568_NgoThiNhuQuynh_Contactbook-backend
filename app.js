3ecb640ae6d2643d6bc75bab9fba8a316862df99
Quynhb1910568 <ngoquynh2908@gmail.com>
2023-10-06 08:06:38 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
-
 app.get("/",(req,res) => {
     res.json({ message: "Welcome to contact book application"});
 });
